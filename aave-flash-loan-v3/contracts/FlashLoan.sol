0a59d0d9fb08c7fd1872c0c675cfabf839010027
Jonathan Spruance <jspruance@inflightcorp.com>
2022-10-02 17:21:47 -0400

    Update imports

--- aave-flash-loan-v3/contracts/FlashLoan.sol
+++ aave-flash-loan-v3/contracts/FlashLoan.sol
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import {FlashLoanSimpleReceiverBase} from "https://github.com/aave/aave-v3-core/blob/master/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
-import {IPoolAddressesProvider} from "https://github.com/aave/aave-v3-core/blob/master/contracts/interfaces/IPoolAddressesProvider.sol";
-import {IERC20} from "https://github.com/aave/aave-v3-core/blob/master/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
 
 contract FlashLoan is FlashLoanSimpleReceiverBase {
     address payable owner;
